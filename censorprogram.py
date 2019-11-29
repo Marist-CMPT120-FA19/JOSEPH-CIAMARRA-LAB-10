2c6499cab08dc017e081ed349b87fb28437b0238
Joseph Ciamarra <Joseph.Ciamarra1@marist.edu>
2019-11-29 11:39:56 -0500

    censor

--- censorprogram.py
+++ censorprogram.py
@@ -5,30 +5,33 @@ def censor(word):
     return word
 
 def main():
-    f= input ("Enter the file to censor: ")
-    tex= open(f, 'r')
-    word.f= input ("Enter the file containing the words: ")
-    cen= open(word.f, 'r')
-    cen.word= cen.read().split()
+    file= input ("Enter the file name to censor: ")
+    text= open(file, 'r')
+    words_file= input ("Enter the file name with the censord words: ")
+    censored= open(words_file, 'r')
+    censorwords= censored.read().split()
 
-    cen.t= ""
+    censoredtext= ""
     for line in text:
-        words = line.split()
+        words=line.split()
+
         for i in range(len(words)):
             word= ""
             for letter in words [i]:
                 if letter.isalpha():
                     word+= letter
-            if word in cen.word:
-                words[i]= cen(words[i])
-        cen.t += " ".join(words) + '\n'
-
-        tex.close()
-        cen.close()
+            if word in censorwords:
+                words[i]= censor(words[i])
+        censoredtext += " ".join(words) + '\n'
 
         newfile = open("censored:" + f, 'w')
         newfile.write(censored_text)
+        text.close()
+        censored.close()
+        
+        newfile=open("censored:" + file, 'w')
+        newfile.write(censoredtext)
         newfile.close()
-main()      
 
+main()
         
